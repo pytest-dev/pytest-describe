baeedc0332d87412a5e30338193add1c21f91edf
Robin Pedersen <robinpeder@gmail.com>
2016-01-11 14:50:05 +0100

    Bump for new release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pytest-describe',
-    version='0.10.2',
+    version='0.10.3',
     description='Describe-style plugin for pytest',
     url='https://github.com/ropez/pytest-describe',
     author='Robin Pedersen',
