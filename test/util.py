28c11c91ad056735952f904c86c2fb726ef90f81
Christoph Zwerschke <cito@online.de>
2020-04-22 01:08:08 +0200

    Remove checks for deprecated keys in outcomes

--- test/util.py
+++ test/util.py
@@ -1,7 +1,7 @@
 def assert_outcomes(result, **expected):
     outcomes = result.parseoutcomes()
 
-    for key in 'seconds', 'pytest-warnings', 'warnings', 'warning':
+    for key in 'seconds', 'warnings':
         if key in outcomes:
             del outcomes[key]
 
