c07a000f4b2601388a917d81532cf0a7fa12362c
Christoph Zwerschke <cito@online.de>
2021-08-18 14:56:35 +0200

    Extend test matrix for pytest 6

--- test/util.py
+++ test/util.py
@@ -1,7 +1,7 @@
 def assert_outcomes(result, **expected):
     outcomes = result.parseoutcomes()
 
-    for key in 'seconds', 'warnings', 'warning':
+    for key in 'seconds', 'warning', 'warnings':
         if key in outcomes:
             del outcomes[key]
 
