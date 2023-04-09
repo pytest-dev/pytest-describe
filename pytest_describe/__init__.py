8b050964df712cb27079cd65abc53c609e387c68
Christoph Zwerschke <cito@online.de>
2023-04-10 00:34:03 +0200

    Use setup.cfg file

--- pytest_describe/__init__.py
+++ pytest_describe/__init__.py
@@ -1,4 +1,5 @@
 from .shared import behaves_like
 
+__all__ = ['behaves_like']
 
-__all__ = ["behaves_like"]
+__version__ = '2.1.0'
--- setup.py
+++ setup.py
@@ -1,45 +1,4 @@
 from setuptools import setup
 
-
-with open("README.rst") as readme_file:
-    readme = readme_file.read()
-
-
-setup(
-    name="pytest-describe",
-    version="2.1.0",
-    description="Describe-style plugin for pytest",
-    long_description=readme,
-    long_description_content_type="text/x-rst",
-    url="https://github.com/pytest-dev/pytest-describe",
-    author="Robin Pedersen",
-    author_email="robinpeder@gmail.com",
-    maintainer="Christoph Zwerschke",
-    maintainer_email="cito@online.de",
-    license="MIT",
-    license_file="LICENSE",
-    platforms=["unix", "linux", "osx", "cygwin", "win32"],
-    install_requires=["pytest>=4.6,<8"],
-    entry_points={
-        "pytest11": ["pytest-describe = pytest_describe.plugin"],
-    },
-    packages=["pytest_describe"],
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: POSIX",
-        "Operating System :: Microsoft :: Windows",
-        "Operating System :: MacOS :: MacOS X",
-        "Topic :: Software Development :: Testing",
-        "Topic :: Software Development :: Libraries",
-        "Topic :: Utilities",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-    ],
-)
+if __name__ == "__main__":
+    setup()
